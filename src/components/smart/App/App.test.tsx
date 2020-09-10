04190bfc4c4f361c810a8e95eace6beccb0a322e
Ivyman <calamberia@gmail.com>
2020-09-10 22:10:49 +0200

    feat(tests): add tests for App component

--- src/components/smart/App/App.test.tsx
+++ src/components/smart/App/App.test.tsx
@@ -3,9 +3,9 @@ import { render } from "@testing-library/react";
 
 import { App } from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/app/i);
+test("render App component", () => {
+  const { container } = render(<App />);
+  const mainElement = container.querySelector("main");
 
-  expect(linkElement).toBeInTheDocument();
+  expect(mainElement).toBeInTheDocument();
 });
