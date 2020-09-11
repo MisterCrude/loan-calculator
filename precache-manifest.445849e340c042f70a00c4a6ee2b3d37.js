996abfa9c00d36c76fa9184edf1796cfba579097
Ivyman <calamberia@gmail.com>
2020-09-11 18:47:42 +0200

    Updates

---
+++ precache-manifest.445849e340c042f70a00c4a6ee2b3d37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e4b2750cad9ce8645d218377862cc4",
+    "url": "/loan-calculator/index.html"
+  },
+  {
+    "revision": "dcabf7427741f132544f",
+    "url": "/loan-calculator/static/js/2.a61260aa.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/loan-calculator/static/js/2.a61260aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ccb7c0fce18918e20dd",
+    "url": "/loan-calculator/static/js/main.7f7dab1e.chunk.js"
+  },
+  {
+    "revision": "53cea0433bee651416e5",
+    "url": "/loan-calculator/static/js/runtime-main.fd18f442.js"
+  }
+]);
