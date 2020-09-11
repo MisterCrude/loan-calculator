ab4e3fa996b16c03487844c6b68b97b666f59d88
Ivyman <calamberia@gmail.com>
2020-09-11 13:02:39 +0200

    feat(tests): add tests for Inputs component

--- src/components/dump/Inputs/Inputs.test.tsx
+++ src/components/dump/Inputs/Inputs.test.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { LOANS } from "@config/constants";
 
 import { Inputs } from "./Inputs";
 
-test("renders learn react link", () => {
-  // const { getByText } = render(<Inputs />);
-  // const linkElement = getByText(/Inputs/i);
-  // expect(linkElement).toBeInTheDocument();
+test("render Inputs with default values", () => {
+  const stubFunction = () => {};
+
+  const { container } = render(
+    <Inputs
+      loanType={LOANS[1]}
+      loanAmount={1000}
+      yearsAmount={4}
+      onSetLoanType={stubFunction}
+      onChangeLoanAmount={stubFunction}
+      onChangeYearsAmount={stubFunction}
+    />
+  );
+  const loanAmountElement = container.querySelector("#loanAmountRange") as Element;
+  const yearsAmountElement = container.querySelector("#yearsAmountRange") as Element;
+  const loanTypeElement = container.querySelector("#loanType__input") as Element;
+
+  expect(loanAmountElement).toHaveValue(String(1000));
+  expect(yearsAmountElement).toHaveValue(String(4));
+  expect(loanTypeElement).toHaveValue("instance");
 });
